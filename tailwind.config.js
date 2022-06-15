645a57faf01604c257431fd147a551c017937db1
luispeixoto <luispeixoto@pq.uenf.br>
2022-06-15 19:34:01 -0300

    feat: adicionado tema global

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: false,
+    themes: [
+      {
+        mytheme: {
+          "default": "#fafafa",
+          "primary": "#01579b",
+          "secondary": "#D926A9",
+          "accent": "#1FB2A6",
+          "neutral": "#01579b",
+          "base-100": "#ffffff",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
   },
 };
