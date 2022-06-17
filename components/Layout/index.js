d7aa1f86a90addd51914dce79e712f75a2add2c8
Guilherme <guilhermet213@gmail.com>
2022-06-17 09:43:08 -0300

    feat: adicionado componente seo #3

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
+import { Seo } from "../Seo";
 
 export const Layout = ({ children }) => {
   return (
     <>
+      <Seo />
       <Header />
       {children}
       <Footer />
