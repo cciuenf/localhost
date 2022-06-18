e3f99690252c4fd0280d48b79d43b2a3584be804
Guilherme <guilhermet213@gmail.com>
2022-06-17 22:35:30 -0300

    feat(strapi): adicionado plugin de upload do cloudinary

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,6 +1,21 @@
-module.exports = () => {
+module.exports = ({ env }) => {
   return {
     ckeditor: true,
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
     slugify: {
       enabled: true,
       config: {
