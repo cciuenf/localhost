a5e788e7c428678965705a131deab0917b99b611
luispeixoto <luispeixoto@pq.uenf.br>
2022-06-22 20:05:27 -0300

    feat: adicionado a pagina de documentos #9

---
+++ pages/documentos.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Layout, Section, Document } from "../components";
+import { api } from "../services/api";
+
+const Documentos = (props) => {
+  return (
+    <Layout seo={props.seo}>
+      <Section title={props.title}>
+        {props.documents.map((document, index) => (
+          <Document
+            key={index}
+            title={document.title}
+            urlDownloadAll={"url de download todos os anexos"}
+            attachments={[
+              { title: "Titulo", url: "url" },
+              { title: "Titulo", url: "url" },
+            ]}
+          >
+            {document.description}
+          </Document>
+        ))}
+      </Section>
+    </Layout>
+  );
+};
+
+export const getStaticProps = async () => {
+  const {
+    data: {
+      data: { attributes: page },
+    },
+  } = await api.get("/pages/5?populate=seo.meta_tags,sections");
+
+  const {
+    data: { data },
+  } = await api.get("/documents");
+
+  const documents = data.map((document) => document.attributes);
+
+  return {
+    props: {
+      ...page,
+      documents,
+    },
+  };
+};
+
+export default Documentos;
